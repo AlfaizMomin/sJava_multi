4e8285bb678796a0f13c74fa7f983ea505cb2502
AlfaizMomin <alfaiz.momin1998@gmail.com>
2025-04-21 16:16:00 +0530

    Create pareesr.java

---
+++ parser/pareesr.java
@@ -0,0 +1,14 @@
+public class BrokenCode {
+
+    public static void main(String[] args) {
+        System.out.println("This line is fine");
+
+        // Missing closing quote and semicolon
+        System.out.println("This line is broken)
+
+        // Missing method closing brace
+    }
+
+    // Extra brace with no matching block
+    }
+}
