821b4d8d34578d645eb970289612060ab522d0de
AlfaizMomin <alfaiz.momin1998@gmail.com>
2025-07-21 17:18:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello{
   public printSTMT(){
     System.out.println("Hello");
 	    System.out.println("Hello");
+	  System.out.println("Hello");
 	System.out.println("Hello");
 	System.out.println("Hello");
 	  System.out.println("Hello");
